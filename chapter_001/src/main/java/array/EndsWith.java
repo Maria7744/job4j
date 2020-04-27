48494329af3ac35b8f357419c7f6bc534ace81a2
Maria7744 <pola7744@mail.ru>
2020-04-27 23:30:01 +0300

    Метод поверяет что слово заканчивается на последовательность

---
+++ chapter_001/src/main/java/array/EndsWith.java
@@ -0,0 +1,17 @@
+package array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int index = 0; index < post.length; index++) {
+            int wordIndex = word.length - index - 1;
+            int postIndex = post.length - index - 1;
+            if (word[wordIndex] != post[postIndex]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
