f99eb37329494365670aa9415326adca29a38895
Maria7744 <pola7744@mail.ru>
2020-04-29 15:48:36 +0300

    Проверка победителя

---
+++ chapter_001/src/main/java/array/MatrixCheck.java
@@ -0,0 +1,48 @@
+package array;
+
+public class MatrixCheck {
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int col = 0; col <= board[row].length; col++) {
+            if (board[row][col] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(char[][] board, int col) {
+        boolean result = true;
+        for (int row = 0; row <= board.length; row++) {
+            if (board[row][col] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for ( int i=0;i<board.length;i++ ){
+            rsl[i] =board[i][i];
+        }
+        return rsl;
+    }
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int index=0;index<board.length;index++ ) {
+            if (board[index][index] == 'X') {
+                if (monoHorizontal(board, index) || (monoVertical(board, index))) {
+
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
