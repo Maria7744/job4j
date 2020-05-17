009d43c191a4e5a0bda967624897c709dea9247a
Maria7744 <pola7744@mail.ru>
2020-05-17 09:05:00 +0300

    Схема прав пользователи и роли new. [#223202]

--- chapter_007/src/main/resources/create.sql
+++ chapter_007/src/main/resources/create.sql
@@ -2,22 +2,25 @@
 create  DATABASE rolesmodeln;
 --создаем таблицу
 use Data rolesmodeln;
-create table comments1 (
-   id serial primary key,
-itemId INT REFERENCES Item(id),
-   name varchar(2000)
+
+
+-- оздаем таблицу state связана по принципу many-to-one с таблицей item
+create table state  (
+id serial primary key,
+	name varchar(2000),
+
 
 );
---выводим ее на экран (перед этим закоментируем предыдущее)
-select * from comments1;
---Создаем следующую таблицу
-create table atachs (
-  id serial primary key,
-itemId INT REFERENCES Item(id),
-  name varchar(2000)
+select * from state;
+----создаем таблицу  category она связана по принципу many-to-one с таблицей item
+create table category (
+id serial primary key,
+	name varchar(2000),
+
 
 );
-select * from atachs;
+select * from category;
+
 --создаем таблицу item в ней прописываем связи по принципу
 --item - comments = one-to-many
         --item - attachs = one-to-many
@@ -38,14 +41,23 @@ name VARCHAR(256) NOT NULL, description TEXT NOT NULL
    insert into item(id, comments1_id, atachs_id,user1_id,category_id) values (1,1,1,1 ,1,1,'онлайм','Заявка на провайдера услуг');
 --read
 select * from item;
---создаем таблицу user1 с ней связана таблица item пол принципу many-to-one
-create table user1(
-id serial primary key,
-	name varchar(2000),
-role_id int references role(id)
+--Создаем следующую таблицу
+create table atachs (
+  id serial primary key,
+itemId INT REFERENCES Item(id),
+  name varchar(2000)
+
 );
--- выводим ее
-select * from user1;
+select * from atachs;
+--создаем таблицу comments1
+create table comments1 (
+   id serial primary key,
+itemId INT REFERENCES Item(id),
+   name varchar(2000)
+
+);
+--выводим ее на экран (перед этим закоментируем предыдущее)
+select * from comments1;
 --создаем таблицу role она связана по принципу many-to-one с таблицей user1
 create table role(
 id serial primary key,
@@ -54,6 +66,16 @@ id serial primary key,
 
 );
 select * from role;
+--создаем таблицу user1 с ней связана таблица item пол принципу many-to-one
+create table user1(
+id serial primary key,
+	name varchar(2000),
+role_id int references role(id)
+);
+-- выводим ее
+select * from user1;
+
+
 --создаем таблицу rules она связана по принципу many-to-many с таблицей role
 create table rules(
 id serial primary key,
@@ -73,18 +95,3 @@ create table role_rules(
 
 
 select * from role_rules;
-----создаем таблицу  category она связана по принципу many-to-one с таблицей item
-create table category (
-id serial primary key,
-	name varchar(2000),
-
-
-);
-select * from category;
--- оздаем таблицу item  связана по принципу many-to-one с таблицей state
-create table state  (
-id serial primary key,
-	name varchar(2000),
-
-
-);
